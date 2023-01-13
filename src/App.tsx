af6f28fef8133b655ab3fae4c4a47e10054798a6
hamza-deriv <hamza.muhammad@regentmarkets.com>
2023-01-13 15:29:23 +0800

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    // <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar />
+      <div className="container">
+        <article>
+          <h1>What is Lorem Ipsum? </h1>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+        </article>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
+    // </div>
   )
 }
 

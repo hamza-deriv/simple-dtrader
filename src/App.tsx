e9b5ec84f2d678eb6bdacdd6d7e57a9ccccc7dc1
hamza-deriv <hamza.muhammad@regentmarkets.com>
2023-01-13 16:19:25 +0800

    websocket  added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');
+
+  const apiCall = {
+    ticks: "R_100",
+    // subscribe: { channel: "order_book_btcusd" },
+  };
+  ws.onopen = (event) => {
+    ws.send(JSON.stringify({ticks:'R_100'}));
+  };
+
+  ws.onmessage = function(msg) {
+    var data = JSON.parse(msg.data);
+    console.log('ticks update: %o', data);
+ };
 
   return (
     // <div className="App">

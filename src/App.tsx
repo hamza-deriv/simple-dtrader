10e13631473f25e843afdf282deef25a7772ba6a
hamza-deriv <hamza.muhammad@regentmarkets.com>
2023-01-16 09:47:40 +0800

    login page v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,19 @@ import { useState, useRef, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { Layout } from "./components/Layout"
+import { AppRouter } from "./routes/AppRouter"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');
-
-  const apiCall = {
-    ticks: "R_100",
-    // subscribe: { channel: "order_book_btcusd" },
-  };
-  ws.onopen = (event) => {
-    ws.send(JSON.stringify({ticks:'R_100'}));
-  };
-
-  ws.onmessage = function(msg) {
-    var data = JSON.parse(msg.data);
-    console.log('ticks update: %o', data);
- };
-
   return (
-    // <div className="App">
-      <div>
-      <Navbar />
-      <div className="container">
-        <article>
-          <h1>What is Lorem Ipsum? </h1>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry...
-        </article>
-      </div>
-    </div>
-    // </div>
+    // <Layout>
+    <>
+       <Navbar />
+     <AppRouter />
+   
+    </>
+     
+    // </Layout>
   )
 }
 
